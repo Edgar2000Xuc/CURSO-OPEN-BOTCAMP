45c2ac5d9acf9ac20af0f839f0f08955382a5514
Edgar2000Xuc <117703302+Edgar2000Xuc@users.noreply.github.com>
2023-04-30 03:43:22 +0330

    Add files via upload

---
+++ LISTAElementos.js
@@ -0,0 +1,13 @@
+const lista = {
+    nombre: "Edgar",
+    edad: 22,
+    soyDesarrollador: true,
+    fechaDeNacimiento: new Date("noviembre 02 2000"),
+    libroFaborito:{
+        titulo: "Nombre del viento",
+        fecha: new Date("2007-03-27"),
+    url: "https://www.goodreads.com/book/show/186074.The_Name_of_the_Wind"
+  }
+};
+    
+console.log(lista.soyDesarrollador);
